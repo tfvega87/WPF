6ebaf78fd5c5e9a176ff64f1acc726388f30451c
Antonio Vega <tfvega@fullsail.edu>
2013-10-11 00:10:09 -0400

    commit assignment

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,17 @@
 /*Antonio Vega Ceja, 10/10/13 */
+
+function calorieIntake () {
+
+    var yourWeight     = parseInt (prompt('What is your weight? (lb):'));
+    var   myCarbs      = parseInt(prompt ('How many carbs do you intake a day:'));
+    var  myAge         = parseInt(prompt('Tell us your age:'))
+    var equationTime   = yourWeight/myCarbs*myAge*100
+
+    var  result        = "I'm "+myAge+" years old, I weigh "+yourWeight+" and I intake "+myCarbs+" amount of carbs a day. Making it a total of "+equationTime+" calories.";
+
+    return equationTime;
+
+}
+
+
+console.log(calorieIntake()) ;
