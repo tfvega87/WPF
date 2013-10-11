d9b971a094993e348c262c760827476d0b7020a2
Antonio Vega <tfvega@fullsail.edu>
2013-10-10 23:58:10 -0400

    added code

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,17 @@
-alert("This is in my js file");
+/*Antonio Vega Ceja, 10/10/13 */
+
+function myscore  () {
+    var levelCap           = 50;
+    var myscoreLevel            =prompt("What level are you?") ;
+    var myscorLevel          =parseInt(prompt("What is the highest level"+" max is:"+myscorLevel));
+    var myscoreTotal          = gearLevel+5;
+    var returnMessage  ='next';
+
+if(myLevel>=45 && gearTotal>=40)     {returnMessage="Joining dungeon..."+"You are level "+myscorLevel+ " and your Myscore level is, "+myscoreTotal+"."}
+else                                {returnMessage="You do not meet the requirements."}
+
+    return returnMessage;
+
+}
+
+console.log(myscore());
