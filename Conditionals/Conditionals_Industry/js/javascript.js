4edf5abe5584ea50aa3537f4137294afff2713a4
Antonio Vega <tfvega@fullsail.edu>
2013-10-17 22:41:43 -0400

    ...

--- Conditionals/Conditionals_Industry/js/javascript.js
+++ Conditionals/Conditionals_Industry/js/javascript.js
@@ -12,4 +12,23 @@ function fixOrder() {
     tax      = parseFloat(document.getElementById("tax").value);
     total    = parseFloat(document.getElementById("total").value);
 
-    
+     numPrice = parseFloat(document.getElementById("cost").value);
+    tax      = parseFloat(document.getElementById("tax").value);
+    total    = parseFloat(document.getElementById("total").value);
+
+    numPrice = numPrice + (tax || numPrice * TAX);
+    total    = numPrice;
+    var dec  = new Number((total+'').split('.')[1])  || 0;
+    total    = document.getElementById("total").value =
+                  "$" + Number(parseInt(total)
+                      + '.'
+                      + Math.round(
+                          ( (dec+''.split('').reverse().join('') )/100 + '').split('').reverse().join('')
+                        )).toFixed(2);
+                        );
+
+    if (isNaN(numPrice)) {
+        alert("Sorry,you must enter a numeric value to place order");
+        numPrice = 0;
+    }
+}
