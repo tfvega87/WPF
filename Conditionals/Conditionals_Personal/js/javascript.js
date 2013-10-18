7793fc2ba054375f474b3b0885e223f126814e29
Antonio Vega <tfvega@fullsail.edu>
2013-10-17 23:15:51 -0400

    added calculations

--- Conditionals/Conditionals_Personal/js/javascript.js
+++ Conditionals/Conditionals_Personal/js/javascript.js
@@ -1,4 +1,11 @@
 //Antonio Vega-Ceja
 // 10/17/13
 //Calculate Your Monthly Payments
-           
+            function Calc(){ 
+            var a, res;             
+            a = parseFloat(document.monthly.form.value); 
+            res1 = a/1000;
+            res2 = a/500;
+            res3 = a/300;
+
+            
