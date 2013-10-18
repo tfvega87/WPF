580cd8edd6ea212a16b5ee941845fe13a071275a
Antonio Vega <tfvega@fullsail.edu>
2013-10-17 20:46:15 -0400

    indenting

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Antonio Vega-Ceja
 // 10-17-13
 
-var kidHeight =30;
+var kidHeight = 30;
 
 
-if(kidHeight >48){
-	console.log ("You can ride the coaster!")
+if (kidHeight > 48) {
+        console.log("You can ride the coaster!");
 }
