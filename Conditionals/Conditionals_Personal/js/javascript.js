b92e80a30ae6dedcab585487f3cc595c3ba611bd
Antonio Vega <tfvega@fullsail.edu>
2013-10-17 23:17:07 -0400

    finish code

--- Conditionals/Conditionals_Personal/js/javascript.js
+++ Conditionals/Conditionals_Personal/js/javascript.js
@@ -8,11 +8,17 @@
             res2 = a/500;
             res3 = a/300;
 
-             function Calc(){ 
-            var a, res;             
-            a = parseFloat(document.monthly.form.value); 
-            res1 = a/1000;
-            res2 = a/500;
-            res3 = a/300;
-
-            
+           
+            if(document.monthly.payments.value == "1" )
+            {
+                window.alert("Your monthlys repayments have been estimaded to be $" + res1);
+            }
+            else if(document.monthly.payments.value == "2" )
+            {
+                window.alert("Your monthly repayments have been estimaded to be $ " + res2);
+            }
+            else if(document.monthly.payments.value == "3" )
+            {
+                window.alert("Your monthly repayments have been estimaded to be $ " + res3);
+            } 
+        }
