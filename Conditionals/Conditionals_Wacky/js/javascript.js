a8073052e04089559646e41f6eeab593304c3c5d
Antonio Vega <tfvega@fullsail.edu>
2013-10-17 23:31:30 -0400

    added code

--- Conditionals/Conditionals_Wacky/js/javascript.js
+++ Conditionals/Conditionals_Wacky/js/javascript.js
@@ -2,3 +2,13 @@
 //10/17/13
 
 //Greater than form
+
+function validateForm() {
+    var x = 1300;
+    var y = 999;
+
+    if (x > y) {
+        alert("Sorry, you don't have enough points");
+        return false;
+    }
+}
