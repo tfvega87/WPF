ef944a3bf23f142e8a3db95c538c95936309d366
Antonio Vega <tfvega@fullsail.edu>
2013-10-17 23:16:21 -0400

    added calculation

--- Conditionals/Conditionals_Personal/js/javascript.js
+++ Conditionals/Conditionals_Personal/js/javascript.js
@@ -8,4 +8,11 @@
             res2 = a/500;
             res3 = a/300;
 
+             function Calc(){ 
+            var a, res;             
+            a = parseFloat(document.monthly.form.value); 
+            res1 = a/1000;
+            res2 = a/500;
+            res3 = a/300;
+
             
