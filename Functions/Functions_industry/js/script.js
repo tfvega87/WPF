4e2e2eebb2eded2b4555a704305cc9471e5ec89d
Antonio Vega <tfvega@fullsail.edu>
2013-10-24 23:42:39 -0400

    added industry code and comments

--- Functions/Functions_industry/js/script.js
+++ Functions/Functions_industry/js/script.js
@@ -1,3 +1,19 @@
 /*Antonio Vega-Ceja
-10/24/13 */
+10/24/13 
 
+functions-Industry */
+
+
+
+
+function myRunningTime(startTime, daytime){
+    var returnValue =  "Started running at " +  startTime +" in the " + daytime +"."; 
+     return returnValue;
+     }
+ var startPrompt =  prompt("What was your full running lap time? Enter hour started |hours mins,  seconds ");
+ var place = prompt( "What place?" )
+ 
+ 
+ console.log(myRunningTime(startPrompt,place));
+
+/* calculating the time ran by day time and startime also given the name of the location where this was performed */
