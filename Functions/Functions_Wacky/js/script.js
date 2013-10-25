73907d689a51838d2813419f56c0a36185db676e
Antonio Vega <tfvega@fullsail.edu>
2013-10-24 23:20:40 -0400

    added comment to my file

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -12,3 +12,4 @@ if (redBull >= 5) {
         console.log("Getting Dizzy and Jittery!")
 }
 
+/* Very simple operation that shows if a person consumes 5 or more Redbull's(energy drinks) in a day they would have heart problems that can cause illness or death, if they consume 2 or less they are ok and in no danger of heart problems and if they consume 3 or 4 in a day they would start to feel a sugar rush that can make there body feel less motivated and weak the rest of the day */
