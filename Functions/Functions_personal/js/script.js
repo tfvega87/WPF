11d92649c82c0e95ff9a19707554138e39af3a59
Antonio Vega <tfvega@fullsail.edu>
2013-10-24 23:51:12 -0400

    added comment and code to personal functions

--- Functions/Functions_personal/js/script.js
+++ Functions/Functions_personal/js/script.js
@@ -5,3 +5,9 @@ Functions-Personal */
 
 
 
+var scrap = 320;
+var complete= 980;
+        console.log("Rolls that I make every day " + (complete > 300  "complete rolls than " + scrap));
+        
+        
+        /* I complete about 980 rolls every day at my work than the scrap I leave behing from the material I run */
