6c9d8031072f62850b76565b074df18648aade24
Antonio Vega <tfvega@fullsail.edu>
2013-10-24 23:16:44 -0400

    added my wacky functions code

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -1,3 +1,14 @@
 /*Antonio Vega-Ceja
-10/24/13 */
+10/24/13 
+
+Functions-Wacky*/
+
+
+if (redBull >= 5) {
+        console.log("On Your way to a heart attack!");
+} else if (redBull <= 2) {
+        console.log("Energy motivated!");
+}else {
+        console.log("Getting Dizzy and Jittery!")
+}
 
